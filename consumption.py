78a543e910a04a551aa5d1a6534f2835911660d7
Alan Kregness <apkregness@gmail.com>
2017-12-02 17:03:44 -0600

    Completed through returning json of departure times

--- consumption.py
+++ consumption.py
@@ -1,17 +1,13 @@
-import requests
-import json
 from sys import argv
-from difflib import SequenceMatcher
 
-DIRECTIONS = {
-    "south" : 1,
-    "east"  : 2,
-    "west"  : 3,
-    "north" : 4
-}
-payload = {'format':'json'}
+import requests
+
+#API URLs and payload info
 get_routes_url = 'http://svc.metrotransit.org/NexTrip/Routes'
-get_stops_url = 'http://svc.metrotransit.org/NexTrip/Stops/{ROUTE}/{DIRECTION}'
+get_directions_url = 'http://svc.metrotransit.org/NexTrip/Directions/{route}'
+get_stops_url = 'http://svc.metrotransit.org/NexTrip/Stops/{route}/{direction}'
+get_departures_url = 'http://svc.metrotransit.org/NexTrip/{route}/{direction}/{stop}'
+payload = {'format':'json'}
 
 #Check for exactly 3 arguments after script name on the command line
 if len(argv) != 4:
@@ -20,17 +16,44 @@ if len(argv) != 4:
 else:
     script, req_route, req_stop, req_direction = argv
 
-#Check if valid direction
-if req_direction.lower() not in DIRECTIONS:
-    print('\'{asdf}\' is not a valid direction. Please enter as "north", "south", "east", or "west".'.format(asdf=direction))
+#Use a generator to list conversion to find a match for the requested route
+routes_json = requests.get(get_routes_url, params=payload).json()
+routes = list((route for route in routes_json if req_route.lower() in route['Description'].lower()))
+
+#If more than 1 potential route matches, display routes so user can rerun script with desired route
+if len(routes) > 1:
+    print('More than one possible route found, please rerun script using the desired route:')
+    print('\n'.join(route['Description'] for route in routes))
     quit()
 
-#Needs to be an existing route
-routes = requests.get(get_routes_url, params=payload).json()
+#No valid routes found
+elif len(routes) <= 0:
+    print('Route \'{}\' not found or not in service today.'.format(req_route))
+    quit()
 
-#for route in routes:
-#    if SequenceMatcher(None, req_route, route['Description']).ratio() > .75:
-#        print(route)
+#If we reach this point, one route has been found for the input
+route = routes[0]['Route']
 
-routeID = (route['Route'] for route in routes if req_route in route['Description']).__next__()
-print(routeID)
+#Now we validate that the route goes in the entered direction
+directions_json = requests.get(get_directions_url.format(route=route), params=payload).json()
+try:
+    direction = (direction['Value'] for direction in directions_json if req_direction.lower() == direction['Text'][:-5].lower()).__next__()
+except StopIteration as stopiter:
+    print('The direction you entered is not valid for the given route.')
+    print('For this route, enter either {x} or {y}.'.format(x=directions_json[0]['Text'][:-5].lower(), y=directions_json[1]['Text'][:-5].lower()))
+    quit()
+
+#Now we validate that the entered bus stop exists on the route, again using a generator
+stops_json = requests.get(get_stops_url.format(route=route, direction=direction), params=payload).json()
+try:
+    stop = (stop['Value'] for stop in stops_json if req_stop.lower() == stop['Text'].lower()).__next__()
+except StopIteration as stopiter:
+    print('No bus stop named \'{stop}\' found on your entered route.'.format(stop=req_stop))
+    print('The bus stops on your entered route are as follows:')
+    print('\n'.join(stop['Text'] for stop in stops_json))
+    quit()
+
+#If we reach this point, we have a valid route, stop, and direction
+print(route, stop, direction)
+departures_json = requests.get(get_departures_url.format(route=route, direction=direction, stop=stop), params=payload).json()
+
